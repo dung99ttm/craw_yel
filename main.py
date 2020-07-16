0d5768224324e81f761cc8f3aff08018711095ce
kad <nhabachocdung@gmail.com>
2020-07-16 13:40:56 +0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from controllers import ControllerRestaurant
+"""from controllers import ControllerRestaurant
 
 if __name__ == "main":
     a = ControllerRestaurant()
 
     list_url = a.list_link_restaurant()
-    a.restaurant(list_url)
+    a.restaurant(list_url)"""
